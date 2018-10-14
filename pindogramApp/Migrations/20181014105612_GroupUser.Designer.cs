b071647d5406203e778364ad1c68fab20af78948
janek <jankaczor6@wp.pl>
2018-10-14 12:59:55 +0200

    Api fix 2

--- pindogramApp/Migrations/20181014105612_GroupUser.Designer.cs
+++ pindogramApp/Migrations/20181014105612_GroupUser.Designer.cs
@@ -10,7 +10,7 @@ using pindogramApp.Entities;
 namespace pindogramApp.Migrations
 {
     [DbContext(typeof(PindogramDataContext))]
-    [Migration("20181012193246_GroupUser")]
+    [Migration("20181014105612_GroupUser")]
     partial class GroupUser
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -33,6 +33,46 @@ namespace pindogramApp.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Groups");
+                });
+
+            modelBuilder.Entity("pindogramApp.Entities.Meme", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<int>("AuthorId");
+
+                    b.Property<DateTime>("DateAdded");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AuthorId");
+
+                    b.ToTable("Memes");
+                });
+
+            modelBuilder.Entity("pindogramApp.Entities.MemeRate", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<int?>("MemeId");
+
+                    b.Property<int?>("UserId");
+
+                    b.Property<bool>("isUpvote");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("MemeId");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("MemeRates");
                 });
 
             modelBuilder.Entity("pindogramApp.Entities.User", b =>
@@ -59,6 +99,25 @@ namespace pindogramApp.Migrations
                     b.HasIndex("GroupId");
 
                     b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("pindogramApp.Entities.Meme", b =>
+                {
+                    b.HasOne("pindogramApp.Entities.User", "Author")
+                        .WithMany()
+                        .HasForeignKey("AuthorId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("pindogramApp.Entities.MemeRate", b =>
+                {
+                    b.HasOne("pindogramApp.Entities.Meme", "Meme")
+                        .WithMany()
+                        .HasForeignKey("MemeId");
+
+                    b.HasOne("pindogramApp.Entities.User", "User")
+                        .WithMany()
+                        .HasForeignKey("UserId");
                 });
 
             modelBuilder.Entity("pindogramApp.Entities.User", b =>
