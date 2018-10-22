d8fe8450f9c30e57e99923ec7772214f76cfa3b8
bartkopolak <pangogo9001@gmail.com>
2018-10-23 01:53:54 +0200

    Added Comment entity, created controller and service for comments

---
+++ pindogramApp/Dtos/CommentDto.cs
@@ -0,0 +1,15 @@
+﻿using pindogramApp.Entities;
+using System;
+
+
+namespace pindogramApp.Dtos
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime DateAdded { get; set; }
+        public AuthorDto Author { get; set; }
+        public int MemeId { get; set; }
+    }
+}
---
+++ pindogramApp/Dtos/CreateCommentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace pindogramApp.Dtos
+{
+    public class CreateCommentDto
+    {
+        public string Content { get; set; }
+        public int MemeId { get; set; }
+    }
+}
