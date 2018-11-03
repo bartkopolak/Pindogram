0bc532f823ecb7f091ec4abea7495e4b576993b1
Erni <eresv6@gmail.com>
2018-11-03 12:20:13 +0100

    wykresy frontend

--- pindogramApp/ClientApp/src/app/entities/administration/administration.component.ts
+++ pindogramApp/ClientApp/src/app/entities/administration/administration.component.ts
@@ -7,6 +7,7 @@ import { MemesService } from '../../home';
 import { Memes } from '../../home/memes/memes';
 import { AlertService } from '../../shared/alert/alert.service';
 import { DeleteMemesComponent } from '../../home/memes/delete-memes.component';
+
 @Component({
   selector: 'app-administration',
   templateUrl: './administration.component.html',
@@ -19,6 +20,64 @@ export class AdministrationComponent implements OnInit, OnDestroy {
 
   memes: Memes[];
   subscription: Subscription;
+  memeActivated = [
+    {
+      'name': 'Zatwierdzone memy',
+      'series': [
+        {
+          'name': '2018-10-17',
+          'value': 4
+        },
+        {
+          'name': '2018-10-18',
+          'value': 0
+        },
+        {
+          'name': '2018-10-19',
+          'value': 1
+        },
+        {
+          'name': '2018-10-20',
+          'value': 10
+        },
+        {
+          'name': '2018-10-21',
+          'value': 4
+        },
+        {
+          'name': '2018-10-22',
+          'value': 0
+        },
+        {
+          'name': '2018-10-23',
+          'value': 1
+        },
+        {
+          'name': '2018-10-24',
+          'value': 10
+        },
+        {
+          'name': '2018-10-25',
+          'value': 1
+        },
+        {
+          'name': '2018-10-26',
+          'value': 10
+        }
+      ]
+    }
+  ];
+  memeRates: any = {};
+  viewBar: any[] = [200, 400];
+  viewLine: any[] = [1000, 400];
+  showXAxis = true;
+  showYAxis = true;
+  showXAxisLabel = true;
+  showYAxisLabel = true;
+  showGridLines = false;
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28']
+  };
 
   constructor(
     private memesService: MemesService,
@@ -35,6 +94,21 @@ export class AdministrationComponent implements OnInit, OnDestroy {
     this.memesService.getAllUnapproved().subscribe(
       (res: HttpResponse<Memes[]>) => {
         this.memes = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+
+    this.memesService.getNumerOfAllLikesAndDislikes().subscribe(
+      (res: HttpResponse<any>) => {
+        this.memeRates = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+
+    this.memesService.getDateToNumberOfApprovedMemes().subscribe(
+      (res: HttpResponse<any>) => {
+        console.log('res', res);
+        // this.memeActivated = res.body;
       },
       (res: HttpErrorResponse) => this.onError(res.message)
     );
